5252870cbc1b24f4bf8fca35918db16ba7cece88
vinicius_menendez <vinnymnascimento@hotmail.com>
2024-07-01 18:32:46 -0300

    adicionei como comentario outra forma de fazer o exercicio

--- atvddtype/exercicio3dia11.ts
+++ atvddtype/exercicio3dia11.ts
@@ -15,3 +15,14 @@ numeros.add(readlinesync.questionInt("\nDigite um numero: "));
 
 console.table(numeros);
 // console.log(numeros.values());
+
+
+// OU ******************
+// const listaNumeros: <number> = new Set<number>();
+// for(let i = 0; i < 10; i++){
+//     let numero = readlinesync.questionInt(`Digite${i + 1}numero inteiro: `);
+//     listaNumeros.add(numero);
+// }
+// console.log("lista de dados do set: ");
+// listaNumeros.forEach(numero => console.log(numero))
+
